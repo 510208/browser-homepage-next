c93317f97d9ed7d48bf8771e11fd6b49ff375b05
SamHacker <xux510208@gmail.com>
2026-06-19 21:05:54 +0800

    🎨 style(RightTop): 調整右上容器的邊距以改善佈局

--- src/components/container/RightTop.vue
+++ src/components/container/RightTop.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Right Top Container -->
   <div
-    class="absolute top-[30px] right-15 flex -translate-x-1/2 transform flex-col items-center gap-5"
+    class="absolute top-[30px] right-6 flex -translate-x-1/2 transform flex-col items-center gap-5"
   >
     <slot></slot>
   </div>
