fb69386fd2cf2717a32fc05743b0c61d6f4e45e5
SamHacker <xux510208@gmail.com>
2026-06-20 07:54:24 +0800

    ✨ feat(LeftTop): 新增 LeftTop 元件以增強 UI 結構

---
+++ src/components/container/LeftTop.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- Left Top Container -->
+  <div
+    class="absolute top-[30px] left-6 flex -translate-x-1/2 transform flex-col items-center gap-5"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
