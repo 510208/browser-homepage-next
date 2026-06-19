ed8ae347363a04b497d8e9b90f4cccd307efd08c
SamHacker <xux510208@gmail.com>
2026-06-19 14:07:40 +0800

    💄 style(BottomCenter): 調整 BottomCenter 組件的樣式以居中對齊內容 ✨ feat(SearchBox): 新增搜尋框功能並更新樣式以改善使用者體驗

--- src/components/container/BottomCenter.vue
+++ src/components/container/BottomCenter.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="absolute bottom-15 left-1/2 flex -translate-x-1/2 transform flex-col gap-5">
+  <div
+    class="absolute bottom-15 left-1/2 flex -translate-x-1/2 transform flex-col items-center gap-5"
+  >
     <slot></slot>
   </div>
 </template>
--- src/components/home/SearchBox.vue
+++ src/components/home/SearchBox.vue
@@ -1,10 +1,22 @@
 <template>
   <Input
-    placeholder="Search..."
-    class="font-300 px-auto max-h-none w-full max-w-[300px] border-none py-2.5 text-white"
+    placeholder="今天要找些什麼呢"
+    v-model="searchQuery"
+    @keyup.enter="doSearch"
+    class="px-auto h-11 max-h-none w-full max-w-[300px] border-none bg-white/20 py-2.5 text-center text-base! font-light placeholder:text-base placeholder:font-light"
   />
 </template>
 
 <script setup lang="ts">
 import { Input } from "@/components/ui/input";
+import { ref } from "vue";
+
+const searchQuery = ref("");
+
+function doSearch() {
+  if (searchQuery.value.trim() !== "") {
+    const query = encodeURIComponent(searchQuery.value.trim());
+    window.open(`https://www.google.com/search?q=${query}`, "_blank");
+  }
+}
 </script>
