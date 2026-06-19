97be516369b357d9f1d7850b11fea230d556829e
SamHacker <xux510208@gmail.com>
2026-06-19 13:54:51 +0800

    ✨ feat(Quote): 新增名言顯示與刷新功能

---
+++ src/components/home/Quote.vue
@@ -0,0 +1,42 @@
+<template>
+  <p id="sh-quote-text" class="font-cyly text-subtitle">{{ quote }}</p>
+</template>
+
+<style scoped>
+#sh-quote-text {
+  font-style: normal;
+  font-weight: 400;
+  line-height: 110%; /* 35.2px */
+}
+</style>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { fetchQuote } from "@/lib/fetchQuote";
+
+const quote = ref("載入中...");
+
+onMounted(async () => {
+  try {
+    quote.value = await fetchQuote();
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+  }
+});
+
+// 點擊文本刷新
+function refreshQuote() {
+  fetchQuote()
+    .then((newQuote) => {
+      quote.value = newQuote;
+    })
+    .catch((error) => {
+      console.error("Error fetching quote:", error);
+    });
+}
+
+const quoteElement = document.getElementById("sh-quote-text");
+if (quoteElement) {
+  quoteElement.addEventListener("click", refreshQuote);
+}
+</script>
