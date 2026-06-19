23d1419c36bc89995c7a09c868d7219fe612e707
SamHacker <xux510208@gmail.com>
2026-06-19 14:19:49 +0800

    ✨ feat(avatar): 新增 Avatar 組件及其相關檔案

---
+++ src/components/widgets/avatar/Avatar.vue
@@ -0,0 +1,13 @@
+<template>
+  <Avatar>
+    <AvatarImage
+      src="https://gravatar.com/avatar/f7598bd8d4aba38d7219341f81a162fc842376b3b556b1995cbb97271d9e2915?v=1753291388000&size=50&d=initials"
+      alt="@510208"
+    />
+    <AvatarFallback>SH</AvatarFallback>
+  </Avatar>
+</template>
+
+<script setup lang="ts">
+import { Avatar, AvatarImage, AvatarFallback } from "@components/ui/avatar";
+</script>
