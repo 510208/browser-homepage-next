e774227839b22c5b4274ce38acfe06a6c15b7af6
SamHacker <xux510208@gmail.com>
2026-06-19 11:42:00 +0800

    ✨ feat(App): 新增主畫面背景圖片及結構 🎉 feat(Clock): 新增時鐘組件

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div id="sh-main" class="h-screen w-screen"></div>
+
+  <div id="sh-background" class="fixed top-0 left-0 -z-1 h-screen w-screen">
+    <img
+      src="@assets/bg-images/background.png"
+      alt="Background"
+      class="h-full w-full object-cover opacity-70"
+    />
+  </div>
+</template>
