0472e283640bf82d6c0e77f9214d8a787806baeb
SamHacker <xux510208@gmail.com>
2026-06-20 08:39:55 +0800

    ✨ feat(weather-icons): 新增天氣圖示集以對應不同天氣代碼

---
+++ src/consts/weather/weatherIconSet.ts
@@ -0,0 +1,42 @@
+const WEATHER_ICON_SET: Record<string, string> = {
+  // weather code -> icon file name
+  // FUCK CWA
+  "01": "sunny", // 晴天
+  "02": "sunny_multi_cloud",
+  "03": "few_clouds",
+  "04": "scattered_clouds",
+  "05": "scattered_clouds",
+  "06": "scattered_clouds",
+  "07": "broken_clouds",
+  "08": "rain",
+  "09": "shower_rain",
+  "10": "shower_rain",
+  "11": "rain",
+  "12": "rain",
+  "13": "shower_rain",
+  "14": "shower_rain",
+  "15": "thunderstorm",
+  "16": "thunderstorm",
+  "17": "thunderstorm",
+  "18": "thunderstorm",
+  "19": "sunny", // 晴午後多雲局部雨
+  "20": "broken_clouds", // 多雲午後局部雨
+  "21": "sunny",
+  "22": "broken_clouds",
+  "23": "shower_rain",
+  "24": "mist",
+  "25": "mist",
+  "26": "mist",
+  "27": "mist",
+  "28": "mist",
+  "29": "shower_rain",
+  "30": "shower_rain",
+  "31": "mist",
+  "32": "mist",
+  "33": "thunder",
+  "34": "thunder",
+  "35": "thunder",
+  "36": "thunder",
+  "37": "snow",
+  // 38以下還沒做
+};
