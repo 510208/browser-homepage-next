c1fc12c5f2f0f55b1d1ce60a5d7525294cff17da
SamHacker <xux510208@gmail.com>
2026-06-20 08:13:39 +0800

    ✨ feat(container): 新增容器元件以整合 BottomCenter、LeftTop 和 RightTop

---
+++ src/components/container/index.ts
@@ -0,0 +1,5 @@
+import BottomCenter from "./BottomCenter.vue";
+import LeftTop from "./LeftTop.vue";
+import RightTop from "./RightTop.vue";
+
+export { BottomCenter, LeftTop, RightTop };
--- src/components/widgets/weather/WeatherTrigger.vue
+++ src/components/widgets/weather/WeatherTrigger.vue
@@ -1,5 +1,12 @@
 <template>
-  <div></div>
+  <div class="flex gap-1">
+    <img class="size-12" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// const weatherIconUrl = computed(() => {
+//   if (!props.weatherData) return "";
+//   return `https://openweathermap.org/img/wn/${props.weatherData.icon}@2x.png`;
+// });
+</script>
