bde3e13e7d725dff22c09556becb510be4a27c2c
SamHacker <xux510208@gmail.com>
2026-06-20 22:37:46 +0800

    ♻️ refactor(LeftTop): 移除不必要的 CSS 變換以簡化結構

--- src/components/container/LeftTop.vue
+++ src/components/container/LeftTop.vue
@@ -1,8 +1,6 @@
 <template>
   <!-- Left Top Container -->
-  <div
-    class="absolute top-[30px] left-6 flex -translate-x-1/2 transform flex-col items-center gap-5"
-  >
+  <div class="absolute top-[30px] left-6 flex flex-col items-center gap-5">
     <slot></slot>
   </div>
 </template>
