66c096caf95500da1c5518856b3a070dc974551e
SamHacker <xux510208@gmail.com>
2026-06-20 22:31:08 +0800

    ♻️ refactor(App): 移除不必要的天氣數據設置代碼

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-
 import { Clock } from "@/components/widgets/clock";
 import { SearchBox } from "@/components/widgets/search-box";
 import { Quote } from "@/components/widgets/quote";
@@ -9,13 +7,6 @@ import { Weather } from "@/components/widgets/weather";
 import Bookmarks from "@/components/widgets/bookmarks/Bookmarks.vue";
 
 import { BottomCenter, RightTop, LeftTop } from "@/components/container";
-
-import { fetchWeather } from "@/lib/weather/fetchWeather";
-import { useWeatherStore } from "@/stores/useWeatherStore";
-const weatherStore = useWeatherStore();
-onMounted(async () => {
-  weatherStore.setWeatherData(await fetchWeather());
-});
 </script>
 
 <template>
