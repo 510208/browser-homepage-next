2cc2ec0028a48a5da3afd26b2a51d1a0e1e83026
SamHacker <xux510208@gmail.com>
2026-06-21 09:59:45 +0800

    ✨ feat(trending-badges): 新增 YouTube 數據顯示功能於 TrendingBadgeList

--- src/components/widgets/trending-badges/TrendingBadgeList.vue
+++ src/components/widgets/trending-badges/TrendingBadgeList.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-wrap gap-2.5">
     <TrendingBadge v-for="badge in badges" :key="badge.name" :data="badge.content">
       <template #icon>
-        <component :is="badge.icon" size="24px" />
+        <component :is="badge.icon" size="24px" class="text-brown-500" />
       </template>
     </TrendingBadge>
   </div>
@@ -11,20 +11,26 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import TrendingBadge from "./TrendingBadge.vue";
-import { GitHubIcon } from "vue3-simple-icons";
-import { github } from "@/lib/trending";
+import { GitHubIcon, YouTubeIcon } from "vue3-simple-icons";
+import { github, youtube } from "@/lib/trending";
 
 const badges = ref<any[]>([]);
 
 onMounted(async () => {
   try {
     const githubContent = await github.parseResponse();
+    const youtubeContent = await youtube.parseResponse();
 
     badges.value = [
       {
         name: "GitHub",
         icon: GitHubIcon,
         content: githubContent.followers.toString(),
+      },
+      {
+        name: "YouTube",
+        icon: YouTubeIcon,
+        content: youtubeContent.statistics.subscriberCount.toString(),
       },
     ];
   } catch (error) {
