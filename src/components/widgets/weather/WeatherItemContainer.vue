2c9ecce308e64b403fa4b46e09c85825b2f1a13f
SamHacker <xux510208@gmail.com>
2026-06-21 11:53:27 +0800

    ✨ feat(WeatherItemContainer): 新增天氣項目容器組件

---
+++ src/components/widgets/weather/WeatherItemContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="cn('relative p-2.5', props.class)">
+    <!-- 圖示 -->
+    <div class="fixed right-0 -bottom-[13px]">
+      <slot name="icon" />
+    </div>
+
+    <!-- 內容 -->
+    <div class="flex flex-col gap-1">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+
+const props = defineProps<{
+  class?: string;
+}>();
+</script>
