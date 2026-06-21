5a59c790b484cfc6cc49a6f0bde691b918e03092
SamHacker <xux510208@gmail.com>
2026-06-21 12:22:58 +0800

    fix(WeatherItemContainer): 修正內容層級的 z-index 屬性

--- src/components/widgets/weather/WeatherItemContainer.vue
+++ src/components/widgets/weather/WeatherItemContainer.vue
@@ -1,12 +1,12 @@
 <template>
   <div :class="cn('relative overflow-hidden p-2.5', props.class)">
     <!-- 圖示 -->
-    <div class="absolute right-0 -bottom-[13px]">
+    <div class="absolute right-0 -bottom-[13px] z-0">
       <slot name="icon" />
     </div>
 
     <!-- 內容 -->
-    <div class="flex flex-col gap-0.5">
+    <div class="z-10 flex flex-col gap-0.5">
       <slot />
     </div>
   </div>
