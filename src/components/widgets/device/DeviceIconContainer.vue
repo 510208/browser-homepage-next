2a8dae49f2cba5870889a99b72b9e522e6f90418
SamHacker <xux510208@gmail.com>
2026-06-26 23:18:58 +0800

    ✨ feat(device-icon): 新增 DeviceIconContainer 組件以增強工具提示功能

---
+++ src/components/widgets/device/DeviceIconContainer.vue
@@ -0,0 +1,16 @@
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <slot />
+      </TooltipTrigger>
+      <TooltipContent>
+        <slot name="content" />
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
+
+<script lang="ts" setup>
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+</script>
