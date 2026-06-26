630c9f79425ff939bfde059f15b240dbd3029e54
SamHacker <xux510208@gmail.com>
2026-06-26 23:14:37 +0800

    ✨ feat(container): 新增 RightBottom 組件以增強頁面佈局

---
+++ src/components/container/RightBottom.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- Right Bottom Container -->
+  <div class="absolute right-10 bottom-15 flex flex-col items-center gap-5">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
--- src/components/container/index.ts
+++ src/components/container/index.ts
@@ -2,5 +2,6 @@ import BottomCenter from "./BottomCenter.vue";
 import LeftTop from "./LeftTop.vue";
 import RightTop from "./RightTop.vue";
 import LeftBottom from "./LeftBottom.vue";
+import RightBottom from "./RightBottom.vue";
 
-export { BottomCenter, LeftTop, RightTop, LeftBottom };
+export { BottomCenter, LeftTop, RightTop, LeftBottom, RightBottom };
