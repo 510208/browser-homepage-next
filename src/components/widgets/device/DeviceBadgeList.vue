52421f998df3cdd0440af3258402809fcdce7275
SamHacker <xux510208@gmail.com>
2026-06-27 17:18:05 +0800

    ✨ feat(DeviceBadgeList): 增加裝置資訊更新失敗的錯誤提示

--- src/components/widgets/device/DeviceBadgeList.vue
+++ src/components/widgets/device/DeviceBadgeList.vue
@@ -15,6 +15,7 @@ import BadgeBattery from "./single-device/BadgeBattery.vue";
 import BadgeNetwork from "./single-device/BadgeNetwork.vue";
 
 import type { DeviceDataResponse } from "@/types/deviceDataResponse";
+import { toast } from "vue-sonner";
 
 const deviceData = ref<DeviceDataResponse>();
 let timerId: ReturnType<typeof setInterval> | null = null;
@@ -25,11 +26,19 @@ const updateDeviceInfo = async () => {
     console.log("Device data (updated):", deviceData.value);
   } catch (error) {
     console.error("Failed to fetch device info:", error);
+    throw error;
   }
 };
 
 onMounted(async () => {
-  await updateDeviceInfo();
+  try {
+    await updateDeviceInfo();
+  } catch (error) {
+    toast.error("無法取得裝置資訊，請確認本地端服務是否啟動。\n詳情請查看瀏覽器控制台。");
+    console.warn(
+      "請檢查本地端服務（ https://github.com/510208/browser-homepage-next/tree/main/server ）是否已正確的安裝並啟動。\n若已啟動，請向上檢查可能的問題，或前往儲存庫建立Issue。\n若這個問題是有意的，請在 src/App.vue 中移除或停用 <DeviceBadgeList /> 元件。",
+    );
+  }
 
   timerId = setInterval(updateDeviceInfo, 3000);
 });
