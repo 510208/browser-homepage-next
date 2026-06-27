ea9a39ec44b87fff57608a9eea389d4c78668fdb
SamHacker <xux510208@gmail.com>
2026-06-27 14:44:50 +0800

    ✨ feat(device): 新增 DeviceCard 組件以顯示設備資訊

---
+++ src/components/widgets/device/DeviceCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <HoverCard>
+    <HoverCardTrigger>
+      <slot :class="iconClass" />
+    </HoverCardTrigger>
+    <HoverCardContent>
+      <slot name="content" />
+    </HoverCardContent>
+  </HoverCard>
+</template>
+
+<script lang="ts" setup>
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
+
+defineProps<{
+  iconClass: string;
+}>();
+</script>
--- src/components/widgets/device/index.ts
+++ src/components/widgets/device/index.ts
@@ -1,4 +1,5 @@
 import DeviceBadge from "./DeviceBadge.vue";
 import DeviceBadgeList from "./DeviceBadgeList.vue";
+import DeviceCard from "./DeviceCard.vue";
 
-export { DeviceBadge, DeviceBadgeList };
+export { DeviceBadge, DeviceBadgeList, DeviceCard };
