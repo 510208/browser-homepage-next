d47c4df2ea014c11575479299e779e85ad170d94
SamHacker <xux510208@gmail.com>
2026-06-27 21:57:19 +0800

    ✨ feat(Weather): 新增鍵盤事件以開啟天氣卡片並在5秒後自動關閉

--- src/components/widgets/weather/Weather.vue
+++ src/components/widgets/weather/Weather.vue
@@ -1,5 +1,5 @@
 <template>
-  <HoverCard :openDelay="50" :closeDelay="200">
+  <HoverCard v-model:open="openWeatherCard" :openDelay="50" :closeDelay="200">
     <HoverCardTrigger>
       <WeatherTrigger />
     </HoverCardTrigger>
@@ -12,4 +12,24 @@
 <script setup lang="ts">
 import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
 import { WeatherCard, WeatherTrigger } from "./index";
+import hotkeys from "hotkeys-js";
+import { ref, onMounted, onUnmounted } from "vue";
+
+const openWeatherCard = ref<boolean>(false);
+
+onMounted(() => {
+  hotkeys("w", () => {
+    // 檢查天氣卡片是否已開啟，如果已開啟則不動作，否則開啟並在5秒後關閉
+    if (!openWeatherCard.value) {
+      openWeatherCard.value = true;
+      setTimeout(() => {
+        openWeatherCard.value = false;
+      }, 5000);
+    }
+  });
+});
+
+onUnmounted(() => {
+  hotkeys.unbind();
+});
 </script>
