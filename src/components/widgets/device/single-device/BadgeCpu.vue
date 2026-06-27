a44d7fdfb14a5fe62285d7e880293f93ebda6d7d
SamHacker <xux510208@gmail.com>
2026-06-27 14:54:51 +0800

    ✨ feat(device): 新增 BadgeCpu 組件以顯示 CPU 使用率

---
+++ src/components/widgets/device/single-device/BadgeCpu.vue
@@ -0,0 +1,20 @@
+<template>
+  <DeviceCard>
+    <CpuIcon class="text-brown-500" :size="24" />
+
+    <template #content>
+      <span class="text-sm font-medium">
+        {{ cpuUsage !== undefined ? cpuUsage.toFixed(2) : "N/A" }}%
+      </span>
+    </template>
+  </DeviceCard>
+</template>
+
+<script setup lang="ts">
+import DeviceCard from "../DeviceCard.vue";
+import { CpuIcon } from "@lucide/vue";
+
+const { cpuUsage } = defineProps<{
+  cpuUsage: number | undefined;
+}>();
+</script>
