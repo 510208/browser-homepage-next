4817d6ea3e705d3728faaeefd54908ec83b09256
SamHacker <xux510208@gmail.com>
2026-06-27 16:03:29 +0800

    💄 style(DeviceCard): 更新 HoverCardTrigger 的樣式以改善顯示效果

--- src/components/widgets/device/DeviceCard.vue
+++ src/components/widgets/device/DeviceCard.vue
@@ -1,6 +1,6 @@
 <template>
   <HoverCard :openDelay="50" :closeDelay="200">
-    <HoverCardTrigger>
+    <HoverCardTrigger class="flex items-center justify-start gap-2.5 p-2.5 pb-0">
       <slot :class="iconClass ? iconClass : ''" />
     </HoverCardTrigger>
     <HoverCardContent class="w-[400px] overflow-hidden px-0 py-0" align="end">
