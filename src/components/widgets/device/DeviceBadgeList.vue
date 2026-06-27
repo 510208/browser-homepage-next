8212580303575295dda7f5abbb7d414a04f5cfa4
SamHacker <xux510208@gmail.com>
2026-06-27 17:07:01 +0800

    ✨ feat(DeviceBadgeList): 新增網路狀態顯示元件

--- src/components/widgets/device/DeviceBadgeList.vue
+++ src/components/widgets/device/DeviceBadgeList.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-wrap gap-2.5">
     <BadgeCpu :cpu-data="deviceData?.cpu" />
+    <BadgeNetwork :network-data="deviceData?.network" />
     <BadgeBattery :battery-data="deviceData?.battery" />
   </div>
 </template>
@@ -11,15 +12,8 @@ import { onMounted, onUnmounted, ref } from "vue";
 
 import BadgeCpu from "./single-device/BadgeCpu.vue";
 import BadgeBattery from "./single-device/BadgeBattery.vue";
+import BadgeNetwork from "./single-device/BadgeNetwork.vue";
 
-import {
-  BatteryFullIcon,
-  BatteryMediumIcon,
-  BatteryLowIcon,
-  BatteryWarningIcon,
-  BatteryChargingIcon,
-  PlugZapIcon,
-} from "@lucide/vue";
 import type { DeviceDataResponse } from "@/types/deviceDataResponse";
 
 const deviceData = ref<DeviceDataResponse>();
@@ -29,39 +23,6 @@ const updateDeviceInfo = async () => {
   try {
     deviceData.value = await deviceInfo.fetchDeviceInfo();
     console.log("Device data (updated):", deviceData.value);
-
-    // 電池狀態
-    let batteryIcon, batteryContent, batteryIconClass;
-    if (deviceData.value.battery) {
-      batteryContent = `${deviceData.value.battery.percent}%`;
-      batteryIconClass = "text-brown-500";
-
-      if (deviceData.value.battery.power_plugged) {
-        batteryIcon = BatteryChargingIcon;
-      } else {
-        const batteryLevel = deviceData.value.battery.percent;
-        if (batteryLevel > 75) {
-          // >= 75%
-          batteryIconClass = "text-green-400";
-          batteryIcon = BatteryFullIcon;
-        } else if (batteryLevel > 40) {
-          // 75% - 40%
-          batteryIconClass = "text-yellow-400";
-          batteryIcon = BatteryMediumIcon;
-        } else if (batteryLevel > 10) {
-          // 40% - 10%
-          batteryIconClass = "text-yellow-400 animate-pulse";
-          batteryIcon = BatteryLowIcon;
-        } else {
-          // <= 10%
-          batteryIconClass = "text-red-500 animate-pulse";
-          batteryIcon = BatteryWarningIcon;
-        }
-      }
-    } else {
-      batteryContent = "裝置沒有電池";
-      batteryIcon = PlugZapIcon;
-    }
   } catch (error) {
     console.error("Failed to fetch device info:", error);
   }
