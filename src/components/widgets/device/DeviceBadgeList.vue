456b55d6d264c4bcef7cb5d82ff4a4e36c554600
SamHacker <xux510208@gmail.com>
2026-06-28 21:09:18 +0800

    ♻️ refactor(DeviceBadgeList): 更新設備數據存取邏輯以改善代碼結構

--- src/components/widgets/device/DeviceBadgeList.vue
+++ src/components/widgets/device/DeviceBadgeList.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="flex flex-wrap gap-2.5">
-    <BadgeCpu :cpu-data="deviceData?.cpu" />
-    <BadgeNetwork :network-data="deviceData?.network" />
-    <BadgeBattery :battery-data="deviceData?.battery" />
+    <BadgeCpu :cpu-data="deviceData?.deviceData?.cpu" />
+    <BadgeNetwork :network-data="deviceData?.deviceData?.network" />
+    <BadgeBattery :battery-data="deviceData?.deviceData?.battery" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { deviceInfo } from "@/lib/device-info";
-import { onMounted, onUnmounted, ref } from "vue";
+import { onMounted, onUnmounted } from "vue";
 
 import BadgeCpu from "./single-device/BadgeCpu.vue";
 import BadgeBattery from "./single-device/BadgeBattery.vue";
 import BadgeNetwork from "./single-device/BadgeNetwork.vue";
 
-import type { DeviceDataResponse } from "@/types/deviceDataResponse";
 import { toast } from "vue-sonner";
 
-const deviceData = ref<DeviceDataResponse>();
+import { useDeviceDataStore } from "@/stores/useDeviceDataStore";
+
+const deviceData = useDeviceDataStore();
 let timerId: ReturnType<typeof setInterval> | null = null;
 
 const updateDeviceInfo = async () => {
   try {
-    deviceData.value = await deviceInfo.fetchDeviceInfo();
+    deviceData.setDeviceData(await deviceInfo.fetchDeviceInfo());
     // console.log("Device data (updated):", deviceData.value);
   } catch (error) {
     console.error("Failed to fetch device info:", error);
