9929dd11cd4479f1a2da34a8367101ee9b4105eb
SamHacker <xux510208@gmail.com>
2026-07-02 20:23:03 +0800

    ✨ feat(bookmark-key): 新增 BookmarkKey 元件以顯示書籤鍵

---
+++ src/components/widgets/bookmarks/BookmarkKey.vue
@@ -0,0 +1,9 @@
+<template>
+  <kbd
+    class="flex size-5 items-center justify-center rounded-md border border-brown-300 bg-brown-100 font-mono text-sm font-medium text-brown-700 opacity-60 shadow-sm transition-opacity duration-100 group-hover:opacity-100"
+  >
+    <slot />
+  </kbd>
+</template>
+
+<script setup lang="ts"></script>
